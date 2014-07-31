31280bf43233c6d0281bb62451dbd267916a111c
Miroslav Shubernetskiy <miroslav.shubernetskiy@dealertrack.com>
2014-07-31 18:36:19 -0400

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='multinosetests',
-    version='0.1.1',
+    version='0.2',
     author='Miroslav Shubernetskiy',
     description='Helper utility to run multiple nosetests suites. '
                 'Useful for makefile scripts.',
